1596a824f18ec6d7c355e440152145c9db673ffd
Melinda Nguyen <melindanguyenn@gmail.com>
2020-03-23 16:22:28 -0500

    added validation when adding and searching for ingredients, display name of clicked recipe

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,19 +16,27 @@ class Home extends Component {
 
   addIngredient = () => {
     console.log("adding", this.state.temp);
-    this.setState({
-      ingredients: [...this.state.ingredients, this.state.temp]
-    });
-    this.clearInput();
+    if (this.state.temp === "") {
+      alert("Please enter an ingredient!")
+    } else {
+      this.setState({
+        ingredients: [...this.state.ingredients, this.state.temp]
+      });
+      this.clearInput();
+    }
   }; //assigning uesr's temp input to ingredients array,
   //then clearing the input field
 
   runSearch = () => {
     console.log("searching for recipes with", this.state.ingredients);
-    this.props.dispatch({
-      type: "GET_RECIPES",
-      payload: this.state.ingredients
-    });
+    if (this.state.ingredients.length === 0) {
+      alert("Please add an ingredient!")
+    } else {
+      this.props.dispatch({
+        type: "GET_RECIPES",
+        payload: this.state.ingredients
+      });
+    }
   }; //will send this.state.ingredients to saga, to run a search
   //results stored in a reducer which will be called to display on page
 
@@ -58,11 +66,37 @@ class Home extends Component {
           placeholder="Add an ingredient"
         />
         <button onClick={this.addIngredient}>Add</button>
-          {this.state.ingredients.map((ingredient, index) => (
-            <li key={index} id={index}>
-              {ingredient} <button onClick={this.delete}>x</button>
-            </li>
-          ))}
+        {this.state.ingredients.map((ingredient, index) => (
+          <li key={index} id={index} className="itemList">
+            {ingredient}
+            <button className="listItems" onClick={this.delete}>
+              <svg
+                class="bi bi-x-circle"
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z"
+                  clip-rule="evenodd"
+                />
+                <path
+                  fill-rule="evenodd"
+                  d="M11.854 4.146a.5.5 0 010 .708l-7 7a.5.5 0 01-.708-.708l7-7a.5.5 0 01.708 0z"
+                  clip-rule="evenodd"
+                />
+                <path
+                  fill-rule="evenodd"
+                  d="M4.146 4.146a.5.5 0 000 .708l7 7a.5.5 0 00.708-.708l-7-7a.5.5 0 00-.708 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+          </li>
+        ))}
         <br></br>
         <button onClick={this.runSearch}>Search</button>
         <SearchResult />
--- src/components/RecipeClicked/RecipeClicked.js
+++ src/components/RecipeClicked/RecipeClicked.js
@@ -7,18 +7,36 @@ class RecipeClicked extends Component {
     this.props.dispatch({
       type: "FAVORITE_RECIPE",
       payload: event.target.id
-    })
+    });
   }; //sending the id of displayed recipe to be stored in database when favorite is clicked
 
   render() {
     return (
       <div>
-        <h1>welcome RecipeClicked</h1>
-        <button onClick={this.favorite} id={this.props.id}>Favorite</button>
+        <h1>
+          {this.props.summary.title}{" "}
+          <button className="favoriteButton" onClick={this.favorite} id={this.props.id}>
+            <svg
+              class="bi bi-heart"
+              width="2em"
+              height="2em"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 01.176-.17C12.72-3.042 23.333 4.867 8 15z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+        </h1>
+
         <h4>Ingredients:</h4>
-        {this.props.ingredients.map((ingredient, index) =>
+        {this.props.ingredients.map((ingredient, index) => (
           <li key={index}>{ingredient.name}</li>
-        )}
+        ))}
         {/* {this.props.ingredients.map(ingredient => ingredient.amount.us.map((amount, index) => <p key={index}>{amount.value}</p>))} */}
         <h4>Instructions:</h4>
         {this.props.directions.map(steps =>
